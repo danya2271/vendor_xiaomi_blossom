1d543f05a0d75b818aac0a87678ef3c1bae46bbd
danya2271 <dany2271@yandex.ru>
2025-08-17 14:33:12 +0500

    blossom-vendor.mk: Get back telephony

--- blossom-vendor.mk
+++ blossom-vendor.mk
@@ -1314,6 +1314,13 @@ PRODUCT_PACKAGES += \
     ImsService \
     mediatek-common \
     mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-ims-extension-plugin \
+    mediatek-ims-legacy \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common \
     com.fingerprints.extension \
     android.hardware.gpu@1.0-service \
     android.hardware.wifi.hostapd \
